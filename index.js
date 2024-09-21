27009b0788180a9c54eafd69f2c0581e64e94bd8
ZohaSuhail <157147376+ZohaSuhail@users.noreply.github.com>
2024-09-21 22:35:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import cors from 'cors';
+
 
 const app = express();
 
 // Define the Swiggy API URL
+app.use(cors());
 const SWIGGY_API_URL = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3330529&lng=83.0069298&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
 // Set up a simple GET route
@@ -32,6 +35,33 @@ app.get('/', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch data from Swiggy API' });
   }
 });
+
+app.get('/restaurants/:resId', async (req, res) => {
+  const resId=req.params.resId;
+  try {
+    // Fetch data from Swiggy API
+    const response = await fetch( `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.3330529&lng=83.0069298&restaurantId=${resId}`,{
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', // Header to mimic browser request
+      },
+  });
+
+    // Check if the request was successful
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from Swiggy API. Status: ${response.status}`);
+    }
+
+    // Parse response as JSON
+    const data = await response.json();
+
+    // Send JSON response
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Failed to fetch data from Swiggy API' });
+  }
+});
+
 
 // Start the server
 app.listen(3000, () => {
